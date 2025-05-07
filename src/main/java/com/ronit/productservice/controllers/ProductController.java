99f0b56401ddee01c970811f4b576a4725909836
Ronit Nandwani <ronitnandwani@gmail.com>
2025-05-07 23:20:00 +0530

    Add REST endpoint to greet users by name to understand REST API basics in java

--- src/main/java/com/ronit/productservice/controllers/ProductController.java
+++ src/main/java/com/ronit/productservice/controllers/ProductController.java
@@ -1,4 +1,15 @@
 package com.ronit.productservice.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+    @GetMapping("/sayHello/{name}")
+    public String listProducts(@PathVariable String name) {
+        return "Product List + " + name;
+    }
 }
