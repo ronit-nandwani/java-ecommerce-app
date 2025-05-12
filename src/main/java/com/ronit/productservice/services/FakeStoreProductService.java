3c441db3e46429fa9f07c5496a9d66e6f92b9882
Ronit Nandwani <ronitnandwani@gmail.com>
2025-05-12 22:28:00 +0530

    Implement products retrieval from FakeStore API and update logging level

--- src/main/java/com/ronit/productservice/services/FakeStoreProductService.java
+++ src/main/java/com/ronit/productservice/services/FakeStoreProductService.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -25,7 +26,21 @@ public class FakeStoreProductService implements ProductService {
 
     @Override
     public List<Product> getAllProducts() {
-        return List.of();
+        ResponseEntity<FakeStoreProductDto[]> fakeStoreProductDtoResponse =
+                restTemplate.getForEntity(
+                        "https://fakestoreapi.com/products",
+                        FakeStoreProductDto[].class
+                );
+
+        FakeStoreProductDto[] fakeStoreProductDtos =
+                fakeStoreProductDtoResponse.getBody();
+        List<Product> products = new ArrayList<>();
+
+        for (FakeStoreProductDto fakeStoreProductDto : fakeStoreProductDtos) {
+            products.add(convertFakeStoreProductDtoToProduct(fakeStoreProductDto));
+        }
+
+        return products;
     }
 
     @Override
